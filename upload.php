15fc3d4f6a262abdc58d143ddf91094cbf929615
Maria Broide <afalina21@yandex.ru>
2016-06-11 08:55:20 +0300

    test upload

---
+++ upload.php
@@ -0,0 +1,21 @@
+      <form action="upload.php" method="post" enctype="multipart/form-data">
+      <input type="file" name="filename"><br> 
+      <input type="submit" value="Загрузить"><br>
+      </form>
+<?php
+   if($_FILES["filename"]["size"] > 1024*3*1024)
+   {
+     echo ("Размер файла превышает три мегабайта");
+     exit;
+   }
+   // Проверяем загружен ли файл
+   if(is_uploaded_file($_FILES["filename"]["tmp_name"]))
+   {
+     // Если файл загружен успешно, перемещаем его
+     // из временной директории в конечную
+     move_uploaded_file($_FILES["filename"]["tmp_name"], "/books".$_FILES["filename"]["name"]);
+     echo 'Файл загружен!';
+   } else {
+      echo 'Ошибка загрузки файла';
+   }
+?>
