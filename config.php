48a352d9e629e3aec9d9b820895b71f99b60ecfd
afalina <afalina21@yandex.ru>
2016-06-13 04:11:37 -0400

    Test

--- config.php
+++
@@ -1,8 +0,0 @@
-<?
-define('DATABASE_NAME', 'library');
-define('DATABASE_USER', 'root');
-define('DATABASE_PASSWORD', '4444');
-define('DATABASE_HOST', '127.0.0.1');
-define('DATABASE_CHARSET', 'utf8');
-define('APP_URL', 'http://localhost');
-?>
